323f8ffb04fd47674881bf0aeed96e95b4b0bf09
ktanaka101 <kentanaka101@gmail.com>
2022-12-16 20:54:32 +0900

    wip

--- lib/inline_snapshot.dart
+++ lib/inline_snapshot.dart
@@ -3,6 +3,10 @@
 /// More dartdocs go here.
 library inline_snapshot;
 
+import 'package:stack_trace/stack_trace.dart';
+
 export 'src/inline_snapshot_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+void tryExpect(String actual, String expected) {
+  print(Trace.current(1));
+}
