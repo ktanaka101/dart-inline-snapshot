adbe8993780a3ba313e92b08540483d6300cb8f2
ktanaka101 <kentanaka101@gmail.com>
2022-12-16 19:10:35 +0900

    initial commit

---
+++ example/inline_snapshot_example.dart
@@ -0,0 +1,6 @@
+import 'package:inline_snapshot/inline_snapshot.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/inline_snapshot.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library inline_snapshot;
+
+export 'src/inline_snapshot_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
