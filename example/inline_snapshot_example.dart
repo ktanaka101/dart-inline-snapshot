dbcb3ad1b7da1c4bb665a0e2234716766291c2da
ktanaka101 <kentanaka101@gmail.com>
2022-12-17 11:53:58 +0900

    wip

--- example/inline_snapshot_example.dart
+++ example/inline_snapshot_example.dart
@@ -1,6 +1,16 @@
 import 'package:inline_snapshot/inline_snapshot.dart';
+import 'package:test/test.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  tearDownAll(() async {
+    await Expect.apply();
+  });
+
+  group('A group of tests', () {
+    test('First Test', () {
+      var e = Expect();
+      //             ^replace "actual string" when run `UPDATE_EXPECT dart test`
+      e.eq("actual string");
+    });
+  });
 }
